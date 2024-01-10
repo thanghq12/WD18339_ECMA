0a4edea390e5f56aa201be749479de441a93151a
thanghq12 <thanghq12@fpt.edu.vn>
2024-01-10 18:09:17 +0700

    update project

--- bai3.js
+++ bai3.js
@@ -18,13 +18,25 @@ let [loptruong,...sinhvien] = students;
 let arrayTeacherPoly = [
     {
         name:"Thắng",
-        birth:1996
+        birth:1996,
+        team : 1 // web  , 2 mobile 
     },
     {
         name:"Tú",
-        birth:1993
+        birth:1993,
+        team: 1 //web
+    },
+    {
+        name:"Sơn",
+        birth:1993,
+        team: 2 //mobile 
     },
 ]
+//tạo 2 arrayobject là teacherMobilePoly ,teacherWebPoly
+// có sẵn 3 GV ở 
+//trong mỗi arrayobject // duyệt mảng arrTeacherPoly để tìm 
+// GV vào trong team web nhét vào mảng teacherWebPoly
+// GV nào trong team mobile nhé vào mẩng teacherMobilePoly
 let arrayTeacherIntern = [
     {
         name : "hiển",
@@ -49,4 +61,28 @@ for(let {name,birth,score} of arrayTeacherIntern) {
         arrayTeacherPoly = [...arrayTeacherPoly,{name,birth}]
     }
 }
-console.log(arrayTeacherPoly);
+// console.log(arrayTeacherPoly);
+
+function tich3so(a,b,c) {
+    return a * b * c;
+}
+//sử dụng bình thường 
+tich3so(3,4,5);
+// sử dụng spreed array để truyền vào trong đối số 
+let number = [2,4,5];
+console.log(tich3so(...number));
+//Rest Parameter 
+// dùng để trong trường hợp không biết rõ trong hàm có bao nhiêu đối số 
+function restTest(...args) {
+    console.log(args);
+}
+restTest(1,{id:5},3,4,5,6,7,8,9,10);
+function tinhChuVi(...hcn) {
+    for(let {chieudai,chieurong} of hcn ) {
+        console.log(`diện tích là ${chieudai * chieurong}`);
+        console.log(`chu vi  là ${(chieudai + chieurong) * 2}`);
+    }
+}
+tinhChuVi({chieudai:5,chieurong:6},{chieudai:7,chieurong:8})
+//xây dựng 1 hàm tính diện tích và chu vi của nhiều hĩnh nhật 
+// với mỗi đôi số truyền vào sẽ là 1 object {chieudai:5,chieurong:4}
